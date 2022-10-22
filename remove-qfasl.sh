3c33ecc1655c37e7d062fae4f1c6d8e6562f3775
ams <ams>
2022-10-22 09:09:00 +0000

    remove-qfasl.sh: Add file.

---
+++ remove-qfasl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -v patch/cwarns-*.lisp*
+rm -v site/hsttbl.lisp
+
+find . -iname '*.qfasl' -exec rm -v {} \;
+find . -iname '*.qfasl~' -exec rm -v {} \;
+
+# Restore fonts, and UCINIT QFASL.
+fossil revert demo/tvbgar.qfasl
+fossil revert demo/wormch.qfasl
+fossil revert zmail/narrow.qfasl
+fossil revert fonts/
+fossil revert sys/ucinit.qfasl
