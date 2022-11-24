fab5484b0aa6cb26035350f520b9ed443b8d7134
ams <ams>
2022-11-24 12:36:08 +0000

    patch/cwarns-mit-specific.lisp: Add file.

---
+++ patch/cwarns-mit-specific.lisp
@@ -0,0 +1,11 @@
+
+;System MIT-Specific made by LISPM at 11/24/22 07:02:54  -*-Mode: Lisp; Package: User; Base: 10.-*-
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: IO1; XGP LISP >"
+  '((:COMPILE NIL)))
+
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: SITE; MITSPECIFIC LISP >"
+  '((:COMPILE NIL)))
