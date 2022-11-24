97cd5773fd86e0aae2ebd5e2157f97a135a803b3
ams <ams>
2022-11-24 12:34:50 +0000

    patch/cwarns-distribution.lisp: Add file.

---
+++ patch/cwarns-distribution.lisp
@@ -0,0 +1,8 @@
+
+;System Distribution made by LISPM at 11/24/22 07:22:43  -*-Mode: Lisp; Package: User; Base: 10.-*-
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: DISTRIBUTION; DIST LISP >"
+  '((:COMPILE NIL
+     (FS::MAKE-DISTRIBUTION NIL NIL
+      (COMPILER::FREE-VARIABLE :MISSING-DECLARATION NIL "The variable ~S is used free; assumed special." SI::*LMI-STANDARD-DIRECTORIES*)))))
