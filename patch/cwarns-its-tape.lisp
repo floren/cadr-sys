285cebb83eb5993ceb5fab7a0aec3dc14c01ce76
ams <ams>
2022-11-24 12:36:36 +0000

    patch/cwarns-its-tape.lisp: Add file.

---
+++ patch/cwarns-its-tape.lisp
@@ -0,0 +1,13 @@
+
+;System ITS-Tape made by LISPM at 11/24/22 07:23:18  -*-Mode: Lisp; Package: User; Base: 10.-*-
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: TAPE; PDP10 LISP >"
+  '((:COMPILE NIL
+     (FS::LM-PLIST-TO-ITS-FILENAMES NIL NIL
+      (COMPILER::OBSOLETE :OBSOLETE NIL "~S ~A." GET-FROM-ALTERNATING-LIST "is an obsolete function; use GETF instead")
+      (COMPILER::OBSOLETE :OBSOLETE NIL "~S ~A." GET-FROM-ALTERNATING-LIST "is an obsolete function; use GETF instead")
+      (COMPILER::OBSOLETE :OBSOLETE NIL "~S ~A." GET-FROM-ALTERNATING-LIST "is an obsolete function; use GETF instead")
+      (COMPILER::OBSOLETE :OBSOLETE NIL "~S ~A." GET-FROM-ALTERNATING-LIST "is an obsolete function; use GETF instead"))
+     (FS::LOAD-PDP10-DUMP-MAGTAPE NIL NIL
+      (COMPILER:UNDEFINED-FUNCTION-USED :PROBABLE-ERROR NIL "The undefined function ~S was called" FS::STREAM-COPY-PDP10-QFASL-FILE-TO-PDP10-TAPE)))))
