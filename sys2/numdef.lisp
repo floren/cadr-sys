77b3fbf211a14284780b66ce345ea4a26893816f
ams <ams>
2023-02-14 09:47:58 +0000

    sys2/numdef.lisp: Update to version 12.

--- sys2/numdef.lisp
+++ sys2/numdef.lisp
@@ -193,7 +193,7 @@
 ;;; returns ANS in stronger type -- should we use %numeric-contage???
 (defsubst numeric-contage (ans influencer)
   "Return a number = ANS and of the weakest type stronger than both ANS and INFLUENCER."
-  (+ ans (zero-of-type influencer)))
+  (+ ans (- influencer influencer)))
 
 (defun float-coerce (ans influencer)
   "Return a number = ANS and of the same type as INFLUENCER.
