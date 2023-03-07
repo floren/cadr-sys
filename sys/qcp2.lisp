8c76af6838735134f9c2e6dd420f0342a567fe67
ams <ams>
2023-03-07 22:20:36 +0000

    sys/qcp2.lisp: Revert [a03dbccd69] now that we use SYS; QCP1 LISP 573.

--- sys/qcp2.lisp
+++ sys/qcp2.lisp
@@ -690,10 +690,14 @@ See P2BRANCH.")
 ;;;; Functions to gobble multiple values.
 
 (DEFUN (:PROPERTY MULTIPLE-VALUE-BIND P2) (TAIL DEST)
-  (LET ((VLIST (CAR TAIL)))
-    (LET ((MVTARGET (LENGTH VLIST))
-	  (VARS (SECOND TAIL))
-	  (MVFORM (FOURTH TAIL)))
+  ;; The first "argument" is the multiple-value producing form.
+  (LET ((MVFORM (CAR TAIL))
+	(TAIL (CDR TAIL))   ; Remove that, and we have what looks like
+			    ; the arguments that a LET would have in pass 2.
+	NBINDS)
+    (LET* ((VLIST (CAR TAIL))
+	   (MVTARGET (LENGTH VLIST))
+	   (VARS (SECOND TAIL)))
       ;; Compile the form to leave N things on the stack.
       ;; If it fails to do so, then it left only one, so push the other N-1.
       (AND (P2MV MVFORM 'D-PDL MVTARGET)
@@ -704,13 +708,11 @@ See P2BRANCH.")
       ;; Note that the vlist contains the variables
       ;; in the original order,
       ;; each with an initialization of (%POP).
-      (P2PBIND VLIST (THIRD TAIL))
+      (SETQ NBINDS (P2PBIND VLIST (THIRD TAIL)))
       ;; Record that they were popped. -- 9/8/84
       ;; This is needed because P2PBIND binds PDLLVL.
       (MKPDLLVL (- PDLLVL MVTARGET)))
-    (LET ((VARS (THIRD TAIL))
-	  (BODY (CDDDDR TAIL)))
-      (P2PROG12N (LENGTH BODY) DEST BODY))))
+    (P2LET-INTERNAL (SECOND TAIL) NBINDS TAIL DEST)))
 
 (DEFUN (:PROPERTY NTH-VALUE P2) (TAIL DEST)
   (IF (TYPEP (CAR TAIL) '(INTEGER 0))
