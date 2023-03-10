3c5e07d187de421e6b2bcebd4ffa398f0b8a0510
ams <ams>
2023-03-10 14:37:16 +0000

    eh/ehbpt.lisp: Minor tweaks.

--- eh/ehbpt.lisp
+++ eh/ehbpt.lisp
@@ -1,3 +1,4 @@
 ;;; -*- Mode:LISP; Package:EH; Lowercase:T; Base:10; Readtable:ZL -*-
 
-;;;---!!! TBD
+;;;---!!! File is mentioned, but doesn't exist on the TID/671 tape.
+;;;---!!!   Figure out what goes here.
