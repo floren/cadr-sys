5400327e7d2448907cc98b18b76a032cf8816ad1
ams <ams>
2024-08-03 13:32:15 +0000

    Bump Tape system version.

--- tape/tape-cwarns.lisp
+++ tape/tape-cwarns.lisp
@@ -1,129 +1,156 @@
 
-;System Tape made by fileserver at 7-Nov-88 19:14:50  -*-Mode: Lisp; Package: User; Base: 10-*-
+;System Tape made by AMS at 8/03/24 15:13:34  -*-Mode: Lisp; Package: User; Base: 10.-*-
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: TAPE; LMI-DISK LISP >"
+  '((:COMPILE NIL)))
 
-;;; Written 7-Nov-88 19:14:50 by fileserver at site Gigamos Cambridge
-;;; while running on Djinn from band 1
-;;; with Experimental ZMail 74.15, Experimental Tape 26.4, Experimental System 129.0, Experimental ZWEI 127.0, Experimental Local-File 77.0, Experimental File-Server 26.0, Experimental Lambda-Diag 19.0, Experimental Unix-Interface 16.0, Microcode 1762, SDU Boot Tape 3.14, SDU ROM 102, 11/04.
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: TAPE; STRING-MATCHP LISP >"
+  '((:COMPILE NIL)))
+
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: TAPE; LMI-DMA LISP >"
+  '((:COMPILE NIL)))
 
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TAPE LISP >"
   '((:COMPILE NIL
      (TAPE::PARTITION-SEARCHER NIL NIL
-      (SI::EXTRA-PROCESSORS :IMPLAUSIBLE NIL "A ~S expression failed to give alternatives for ~S" SELECT-PROCESSOR (:FALCON))))))
+      (COMPILER::OBSOLETE :OBSOLETE NIL "~S ~A." PROCESS-RESET "is an obsolete function; Use the :RESET message"))
+     (TAPE::WITH-BUFFER-WIRED NIL NIL
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::NUMBER-OF-PAGES)
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::BUFFER)))))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; ERROR LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TEST LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
-  '#FS::LOGICAL-PATHNAME "SYS: TAPE; TAPEMASTER-DRIVER LISP >"
+  '#FS::LOGICAL-PATHNAME "SYS: TAPE; RTAPE-DEVICE LISP >"
   '((:COMPILE NIL
-     (TM::CCB-TPB-POINTER-BASE NIL NIL
-      (COMPILER::DEFSTRUCT-STYLE-WARNING :NOT-PORTABLE NIL "DEFSTRUCT CCB: contains subslot fields (this may not be portable)")
-      (SI::PREMATURE-WARNING-MARKER NIL NIL "The problems described above were in data preceding the definition of ~S." TM::CCB-TPB-POINTER-BASE))
-     (TM::TPB-LINK-BASE NIL NIL
-      (COMPILER::DEFSTRUCT-STYLE-WARNING :NOT-PORTABLE NIL "DEFSTRUCT TPB: contains subslot fields (this may not be portable)")
-      (SI::PREMATURE-WARNING-MARKER NIL NIL "The problems described above were in data preceding the definition of ~S." TM::TPB-LINK-BASE))
-     (TM::GET-STUPID-UNIT NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with a call to TPB-BANK-SELECT as a BYTE specifier (this may not be portable)")))))
-;-*-Mode: Lisp; Package: User; Base: 10. -*-
-(SI:RELOAD-FILE-WARNINGS
-  '#FS::LOGICAL-PATHNAME "SYS: TAPE; NUPI-SUPPORT LISP >"
-  '((:COMPILE NIL
-     (SI::NUPI-CHECK-STATUS NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 520 as a BYTE specifier (this is not portable)")))))
-;-*-Mode: Lisp; Package: User; Base: 10. -*-
-(SI:RELOAD-FILE-WARNINGS
-  '#FS::LOGICAL-PATHNAME "SYS: TAPE; TAPEMASTER-DEVICE LISP >"
-  '((:COMPILE NIL)))
-;-*-Mode: Lisp; Package: User; Base: 10. -*-
-(SI:RELOAD-FILE-WARNINGS
-  '#FS::LOGICAL-PATHNAME "SYS: TAPE; NUPI-DEVICE LISP >"
-  '((:COMPILE NIL
-     ((:METHOD TAPE::NUPI-DEVICE :INITIALIZE) NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 961 as a BYTE specifier (this is not portable)")
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 1032 as a BYTE specifier (this is not portable)"))
-     (TAPE::NUPI-DEVICE-PRESENT? NIL NIL
-      (SI::EXTRA-PROCESSORS :IMPLAUSIBLE NIL "A ~S expression failed to give alternatives for ~S" SELECT-PROCESSOR (:FALCON))))))
+     ((:METHOD TAPE::RTAPE-DEVICE :READ-TO-DISK) NIL NIL
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::SILENT))
+     ((:METHOD TAPE::RTAPE-DEVICE :WRITE-FROM-DISK) NIL NIL
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::SILENT))
+     ((:METHOD TAPE::RTAPE-DEVICE :COMPARE-TO-DISK) NIL NIL
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::SILENT)))))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; DUMPIT LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; LMFL-FORMAT LISP >"
-  '((:COMPILE NIL
-     ((:METHOD TAPE::LMFL-FORMAT :SPACE-TO-END-OF-THIS-FILE) NIL NIL
-      (SI::EXTRA-PROCESSORS :IMPLAUSIBLE NIL "A ~S expression failed to give alternatives for ~S" SELECT-PROCESSOR (:FALCON))))))
+  '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; RAW-FORMAT LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TANALYZ-FORMAT LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TAR-FORMAT LISP >"
-  '((:COMPILE NIL)))
+  '((:COMPILE NIL
+     ((:METHOD TAPE::TAR-FORMAT :COMPARE-FILE) NIL NIL
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::ERROR-ACTION))
+     ((:METHOD TAPE::TAR-FORMAT :OPEN-FILE) NIL NIL
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." PLIST)
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::CHARACTERS)
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." BYTE-SIZE)
+      (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." TAPE::DIRECTION)))))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; BACKUP LISP >"
-  '((:COMPILE NIL
-     (TAPE::RECURSIVE-FILE-LIST-LOCAL NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB-TEST called with a call to GET as a BYTE specifier (this may not be portable)")))))
+  '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; USER LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; INITIALIZATIONS LISP >"
   '((:COMPILE NIL)))
+
+;-*-Mode: Lisp; Package: User; Base: 10. -*-
+(SI:RELOAD-FILE-WARNINGS
+  '#FS::LOGICAL-PATHNAME "SYS: TAPE; LMI-FRAME-CONSTRAINT-PATCH LISP >"
+  '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TFRAME-MACROS LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TFRAME-DEFS LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TFRAME-PROCESS LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TFRAME-WINDOW LISP >"
-  '((:COMPILE NIL)))
+  '((:COMPILE NIL
+     ((:METHOD TFRAME::TFRAME :COMBINED :DEEXPOSE) NIL NIL
+      (COMPILER::INSTANCE-VARIABLE-USED-IN-INTERNAL-LAMBDA :UNIMPLEMENTED NIL "The ~:[~;special ~]instance variable ~S of flavor ~S
+is being referenced by a lexically closed-over function.
+This will not work outside of the dynamic scope of ~S.~:[
+This problem will be fixed when Mly's brain hurts a little less.~]" NIL TV:SCREEN-ARRAY TFRAME::TFRAME SELF NIL)))))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TFRAME-COMS LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; MTDEFS LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; MTSTR LISP >"
-  '((:COMPILE NIL)))
+  '((:COMPILE NIL
+     ((:METHOD FS::MT-FILEHANDLE :OPEN) NIL NIL
+      (COMPILER::OBSOLETE :OBSOLETE NIL "~S ~A." SI::PUT-ON-ALTERNATING-LIST "is an obsolete function; This function is a crock")
+      (COMPILER::OBSOLETE :OBSOLETE NIL "~S ~A." SI::PUT-ON-ALTERNATING-LIST "is an obsolete function; This function is a crock")))))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; COPY LISP >"
   '((:COMPILE NIL
      (FS::DETERMINE-COPY-DESTINATION NIL NIL
-      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "~S is not a known type specifier" FS::LOCAL-FILE-PATHNAME)
-      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "~S is not a known type specifier" FS::REMOTE-LMFILE-PATHNAME))
+      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "Error expanding type specification ~S for ~S:~%   ~A" (QUOTE FS::LOCAL-FILE-PATHNAME) TYPEP #FERROR :PROPERTY-LIST (:TYPE-SPECIFIER FS::LOCAL-FILE-PATHNAME) :CONDITION-NAMES (FERROR ERROR CONDITION SI::INVALID-TYPE-SPECIFIER) :FORMAT-STRING "~S is not a known type specifier" :FORMAT-ARGS (FS::LOCAL-FILE-PATHNAME))
+      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "Error expanding type specification ~S for ~S:~%   ~A" (QUOTE FS::REMOTE-LMFILE-PATHNAME) TYPEP #FERROR :PROPERTY-LIST (:TYPE-SPECIFIER FS::REMOTE-LMFILE-PATHNAME) :CONDITION-NAMES (FERROR ERROR CONDITION SI::INVALID-TYPE-SPECIFIER) :FORMAT-STRING "~S is not a known type specifier" :FORMAT-ARGS (FS::REMOTE-LMFILE-PATHNAME)))
      (FS::COPY-DESTINATION-EXISTS-P NIL NIL
-      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "~S is not a known type specifier" FS::LOCAL-FILE-PATHNAME)
-      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "~S is not a known type specifier" FS::REMOTE-LMFILE-PATHNAME))
+      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "Error expanding type specification ~S for ~S:~%   ~A" (QUOTE FS::LOCAL-FILE-PATHNAME) TYPEP #FERROR :PROPERTY-LIST (:TYPE-SPECIFIER FS::LOCAL-FILE-PATHNAME) :CONDITION-NAMES (FERROR ERROR CONDITION SI::INVALID-TYPE-SPECIFIER) :FORMAT-STRING "~S is not a known type specifier" :FORMAT-ARGS (FS::LOCAL-FILE-PATHNAME))
+      (COMPILER::BAD-TYPE-SPECIFICATION :IMPLAUSIBLE NIL "Error expanding type specification ~S for ~S:~%   ~A" (QUOTE FS::REMOTE-LMFILE-PATHNAME) TYPEP #FERROR :PROPERTY-LIST (:TYPE-SPECIFIER FS::REMOTE-LMFILE-PATHNAME) :CONDITION-NAMES (FERROR ERROR CONDITION SI::INVALID-TYPE-SPECIFIER) :FORMAT-STRING "~S is not a known type specifier" :FORMAT-ARGS (FS::REMOTE-LMFILE-PATHNAME)))
      (FS::COPY-DIRECTORY-AFTER-DIRECTORY NIL NIL
       (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." FS::SINCE)
       (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." FS::ONLY-LATEST)
@@ -140,43 +167,18 @@
       (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." FS::RECOPY-FILE-ON-EOT)
       (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." FS::SELECTIVE)
       (COMPILER::NOT-USED :IMPLAUSIBLE NIL "The variable ~S is bound but never used." FS::COPY-ONLY)))))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; MTAUX LISP >"
-  '((:COMPILE NIL
-     (FS::STREAM-COPY-PDP10-QFASL-FILE-FROM-TAPE NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 1010 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-COPY-QFASL-FILE-TO-PDP10-TAPE NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 1010 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-COPY-PDP10-FMT-FILE-FROM-TAPE NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 601 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-COPY-PDP10-FMT-FILE-TO-PDP10-TAPE NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 1 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-COPY-PDP10-ASCII-FILE-FROM-TAPE NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 601 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-READ-PDP10-TAPE-HEADER NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 2222 as a BYTE specifier (this is not portable)"))
-     (FS::PDP10-HALF-WORDS NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 2222 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-READ-PDP10-DUMP-HEADER NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 2222 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-READ-PDP10-WORD NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 3410 as a BYTE specifier (this is not portable)"))
-     (FS::STREAM-WRITE-PDP10-WORD NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 3410 as a BYTE specifier (this is not portable)"))
-     (FS::PDP10-GET-ASCII-FROM-ARRAY NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 3507 as a BYTE specifier (this is not portable)"))
-     (FS::PRINT-PDP10-ASCII-WORD NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "LDB called with the numeric constant 3507 as a BYTE specifier (this is not portable)"))
-     (FS::LOAD-PDP10-DUMP-MAGTAPE NIL NIL
-      (COMPILER:UNDEFINED-FUNCTION-USED :PROBABLE-ERROR NIL "The undefined function ~S was called" FS::STREAM-COPY-PDP10-QFASL-FILE-TO-PDP10-TAPE)))))
+  '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; ANSI LISP >"
   '((:COMPILE NIL)))
+
 ;-*-Mode: Lisp; Package: User; Base: 10. -*-
 (SI:RELOAD-FILE-WARNINGS
   '#FS::LOGICAL-PATHNAME "SYS: TAPE; TOPS20 LISP >"
-  '((:COMPILE NIL
-     (TOPS20::READ-WORD NIL NIL
-      (COMPILER::INVALID-BYTE-SPEC :NOT-PORTABLE NIL "DPB called with the numeric constant 1800 as a BYTE specifier (this is not portable)")))))
+  '((:COMPILE NIL)))
